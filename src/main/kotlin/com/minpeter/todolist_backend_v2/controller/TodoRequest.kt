e3bcb501c88a4d68ec56eb12fb6da47d65759066
minpeter <kali2005611@gmail.com>
2021-08-06 00:55:00 +0900

    todolist_b_v2 + todolist_f_v2 합병 사전 정리

--- src/main/kotlin/com/minpeter/todolist_backend_v2/controller/TodoRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.minpeter.todolist_backend_v2.controller
-
-data class TodoRequest (val todoName: String)
--- src/main/kotlin/com/minpeter/todolist_backend_v2/repository/TodoRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.minpeter.todolist_backend_v2.repository
-
-import org.springframework.data.repository.CrudRepository
-
-interface TodoRepository : CrudRepository<Todo, Long>
